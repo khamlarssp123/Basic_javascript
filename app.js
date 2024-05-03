18ca1d170263bec0c9ba85734389c5a36b100211
Khamlar Aoutavisai <Khamlar@tplus.la>
2024-05-03 16:54:50 +0700

    add app.js to githup

---
+++ app.js
@@ -0,0 +1,12 @@
+//console.log("Hello,"+process.argv[2]+"!");
+/*var firstName = 'Sitthiphone';
+var LastName = 'PHANDALA';
+
+console.log(firstName + " " + LastName);
+let className = "API Development by Node";
+console.log(className)
+*/
+const emp_id = 110;
+console.log("Employee ID:" + emp_id);
+emp_id = 111;
+console.log("Employee ID:" + emp_id);
